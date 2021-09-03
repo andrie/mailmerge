9458fc8387a7dcb79eb0a550be2a2a2660487c6e
Maëlle Salmon <maelle.salmon@yahoo.se>
2021-09-03 08:00:09 +0200

    Update mail_merge.R

--- R/mail_merge.R
+++ R/mail_merge.R
@@ -82,8 +82,8 @@ mail_merge <- function(data, message, to_col = "email", send = c("preview", "dra
   msg <- mm_read_message(message)
   
   if(!preview && !confirm) {
-    msg <- paste0("Send ", nrow(data), " emails (", send, ")?")
-    cat(msg)
+    confirmmsg <- paste0("Send ", nrow(data), " emails (", send, ")?")
+    cat(confirmmsg)
     if (yesno()) {
       return(invisible())
     }
